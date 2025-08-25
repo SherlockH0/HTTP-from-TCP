91106fa4aa8a7771a778b6173c56cdacd724d77f
 sherlock <apple@MacBook-Pro-apple.local>
2025-08-25 21:18:09 +0300

    HTTP headers

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,5 +1,41 @@
 package headers
 
+import (
+	"bytes"
+	"fmt"
+	"httpfromtcp/internal/request"
+)
+
+var INVALID_HEADER = fmt.Errorf("invalid header")
+
 type Headers map[string]string
 
-func (h Headers) Parse(data []byte) (n int, done bool, err error)
+func NewHeaders() Headers {
+	return Headers{}
+}
+
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	bufIndex := bytes.Index(data, request.SEPARATOR)
+	if bufIndex == -1 {
+		return 0, false, nil
+	}
+	if bufIndex == 0 {
+		return 0, true, nil
+	}
+
+	line := data[:bufIndex]
+
+	index := bytes.Index(line, []byte(":"))
+	if index == -1 {
+		return 0, false, INVALID_HEADER
+	}
+	fieldName := line[:index]
+	if fieldName[len(fieldName)-1] == ' ' {
+		return 0, false, INVALID_HEADER
+	}
+	fieldName = bytes.Trim(fieldName, " ")
+	value := bytes.Trim(line[index+len(":"):], " ")
+
+	h[string(fieldName)] = string(value)
+	return bufIndex + len(request.SEPARATOR), false, nil
+}
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -1,19 +1,59 @@
 package headers
 
-// Test: Valid single header
-headers := NewHeaders()
-data := []byte("Host: localhost:42069\r\n\r\n")
-n, done, err := headers.Parse(data)
-require.NoError(t, err)
-require.NotNil(t, headers)
-assert.Equal(t, "localhost:42069", headers["Host"])
-assert.Equal(t, 23, n)
-assert.False(t, done)
+import (
+	"testing"
 
-// Test: Invalid spacing header
-headers = NewHeaders()
-data = []byte("       Host : localhost:42069       \r\n\r\n")
-n, done, err = headers.Parse(data)
-require.Error(t, err)
-assert.Equal(t, 0, n)
-assert.False(t, done)
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaders(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// TEST: valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("         Host: localhost:42069         \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 41, n)
+	assert.False(t, done)
+
+	// TEST:  Valid 2 headers with existing headers
+	headers = NewHeaders()
+	data = []byte("Host: localhost:42069\r\nAuthorization: Bearer token\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	n, done, err = headers.Parse(data[n:])
+	assert.Equal(t, "Bearer token", headers["Authorization"])
+
+	// TEST: Valid done
+
+	headers = NewHeaders()
+	data = []byte("\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	assert.True(t, done)
+
+	// TEST: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("Host : localhost :42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+}
